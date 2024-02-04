b83d05911aa4bc3b160126091fcabe81c506ecaf
shivam <76024545+dextroshivam@users.noreply.github.com>
2024-02-04 16:56:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let ele=document.getElementById("level");
+let level=0;
+while(ele.parentNode!=null){
+    level++;
+    ele=ele.parentNode;
+}
+alert(`The level of the element is: ${level}`);
